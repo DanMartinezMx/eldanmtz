59797e01f196597c6763a7c92dfab68808d694a2
Daniel Martinez <sarekmartinez53@gmail.com>
2026-06-19 10:24:10 -0600

    fix: tina build passing with correct token config

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <p>Esa página no existe.</p>
+      <p>¿Qué mira, bobo?.</p>
       <Link href="/">← Volver al inicio</Link>
     </div>
   );
--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/Users/dmartinez4/Documents/code/blog/eldanmtz/tina/__generated__/.cache/1781829037795', url: 'https://content.tinajs.io/2.4/content/84696ac4-5f91-4553-ac13-e07247bd19c0/github/main', token: '5b9166088c2378ab1a4e2c3874d79473a4b5003f', queries,  });
+export const client = createClient({ cacheDir: '/Users/dmartinez4/Documents/code/blog/eldanmtz/tina/__generated__/.cache/1781885883430', url: 'https://content.tinajs.io/2.4/content/84696ac4-5f91-4553-ac13-e07247bd19c0/github/main', token: 'b9f53e48232cf8aabac68bfaf41c17555169f1de', queries,  });
 export default client;
   
