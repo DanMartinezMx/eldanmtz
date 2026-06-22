47373aeec6c5ca3056c5083ff356fbeeaad20cdb
Daniel Martinez <sarekmartinez53@gmail.com>
2026-06-22 17:27:37 -0600

    fix: full tina schema regeneration

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'b9f53e48232cf8aabac68bfaf41c17555169f1de', queries,  });
+export const client = createClient({ cacheDir: '/Users/dmartinez4/Documents/code/blog/eldanmtz/tina/__generated__/.cache/1782170835538', url: 'https://content.tinajs.io/2.4/content/84696ac4-5f91-4553-ac13-e07247bd19c0/github/main', token: 'b9f53e48232cf8aabac68bfaf41c17555169f1de', queries,  });
 export default client;
   
